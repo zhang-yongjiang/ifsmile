643c091ecb43b8dd4986ec5e93461790053ccf5b
JustLoveYang <zhangyj251211@gmail.com>
2016-11-19 20:25:05 +0800

    First commit.

---
+++ app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
@@ -0,0 +1,13 @@
+package app.ifsmile.com.ifsmile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smile);
+    }
+}
