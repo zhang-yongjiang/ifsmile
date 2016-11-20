54ef418479bc795b3861d98a04a8a06432f51ea4
JustLoveYang <zhangyj251211@gmail.com>
2016-11-20 13:44:02 +0800

    third commit

--- app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
+++ app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 public class SmileActivity extends AppCompatActivity {
 
     private Button start;
+    private Button noChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,14 @@ public class SmileActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SmileActivity.this,"you clicked the start",Toast.LENGTH_SHORT).show();
+                Toast.makeText(SmileActivity.this,"欢迎来到IfSmile",Toast.LENGTH_SHORT).show();
+            }
+        });
+        noChoice=(Button)findViewById(R.id.without_choice);
+        noChoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SmileActivity.this,"你没有做出任何选择",Toast.LENGTH_SHORT).show();
             }
         });
     }
