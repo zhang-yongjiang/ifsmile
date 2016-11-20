4efeced24f3d1c7b4d47d20c1d6d056f222a8336
JustLoveYang <zhangyj251211@gmail.com>
2016-11-20 11:59:05 +0800

    second commit.

--- app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
+++ app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
@@ -1,13 +1,25 @@
 package app.ifsmile.com.ifsmile;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SmileActivity extends AppCompatActivity {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smile);
+        start=(Button)findViewById(R.id.start_button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SmileActivity.this,"you clicked the start",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
