93335dae8770bd919728f44e541eb7749c6b5cf5
zhang-yongjiang <zhangyongjiang47@gmail.com>
2016-11-22 20:27:32 +0800

    11.22.2 commit

--- app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
+++ app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
@@ -1,46 +1,26 @@
 package app.ifsmile.com.ifsmile;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class SmileActivity extends AppCompatActivity {
-
-    private Button start;
-    private Button noChoice;
+public class SmileActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_smile);
+        setContentView(R.layout.activity_smile_video);
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-
-        start=(Button)findViewById(R.id.start_button);
-        noChoice=(Button)findViewById(R.id.without_choice);
-
-        //设置开始按钮事件
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toStartActivity=new Intent(SmileActivity.this,StartSmileActivity.class);
-                startActivity(toStartActivity);
-                Toast.makeText(SmileActivity.this,"欢迎来到IfSmile",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //设置下一步按钮事件
-        noChoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent toStartActivity=new Intent(SmileActivity.this,StartSmileActivity.class);
-                startActivity(toStartActivity);
-                Toast.makeText(SmileActivity.this,"你没有做出任何选择",Toast.LENGTH_SHORT).show();
-            }
-        });
+        if (fragment == null) {
+            fragment = new VideoPlayFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
