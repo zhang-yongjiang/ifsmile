38ebe1d2dcd3ba27456d5dfad3346a242bd95e2e
zhang-yongjiang <zhangyongjiang47@gmail.com>
2016-11-23 20:34:33 +0800

    11.23.1 commit

--- app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
+++ app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
@@ -12,7 +12,7 @@ public class SmileActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_smile_video);
+        setContentView(R.layout.fragment_smile_video);
         FragmentManager fm=getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
--- app/src/main/java/app/ifsmile/com/ifsmile/VideoPlayFragment.java
+++ app/src/main/java/app/ifsmile/com/ifsmile/VideoPlayFragment.java
@@ -1,8 +1,13 @@
 package app.ifsmile.com.ifsmile;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by zyj on 16-11-21.
@@ -10,6 +15,42 @@ import android.view.MenuInflater;
 
 public class VideoPlayFragment extends Fragment {
 
+
+    private Button mButtonVideo;
+    private Button mButtonMusic;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.fragment_smile_video,group);
+
+
+        mButtonVideo=(Button)view.findViewById(R.id.my_video_button);
+        mButtonMusic=(Button)view.findViewById(R.id.my_music_button);
+
+
+        mButtonVideo.setOnClickListener(new View.OnClickListener() {        //本地视频
+            @Override
+            public void onClick(View v) {
+                //选择本地视频
+            }
+        });
+        mButtonMusic.setOnClickListener(new View.OnClickListener() {        //本地音乐
+            @Override
+            public void onClick(View v) {
+                //选择本地音乐
+            }
+        });
+
+        return view;
+
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
