83cb9151f91fbf2f507925ca85d7658b90b1da65
zhang-yongjiang <zhangyongjiang47@gmail.com>
2016-11-29 22:20:45 +0800

    11.29.1 commit

--- app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
+++ app/src/main/java/app/ifsmile/com/ifsmile/SmileActivity.java
@@ -2,14 +2,13 @@ package app.ifsmile.com.ifsmile;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Window;
 
 public class SmileActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_smile);
 
 
